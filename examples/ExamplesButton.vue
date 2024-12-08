d55f7f639cd3234a1625182b471629b13899aa48
Terry <terry5@foxmail.com>
2024-12-08 17:35:31 +0800

    feat: button slots and style

--- examples/ExamplesButton.vue
+++ examples/ExamplesButton.vue
@@ -12,18 +12,78 @@
         <SelectMenuItem>
           <SelectMenuButton rounded="medium">
             button
+            <template #append>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right-circle sm-icon"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
+                />
+              </svg>
+            </template>
           </SelectMenuButton>
         </SelectMenuItem>
 
         <SelectMenuItem>
           <SelectMenuButton rounded="large">
+            <template #prepend>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-left-circle sm-icon"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+                />
+              </svg>
+            </template>
             button
           </SelectMenuButton>
         </SelectMenuItem>
 
         <SelectMenuItem>
           <SelectMenuButton rounded="pill">
+            <template #prepend>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-left-circle sm-icon"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+                />
+              </svg>
+            </template>
             button
+            <template #append>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right-circle sm-icon"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
+                />
+              </svg>
+            </template>
           </SelectMenuButton>
         </SelectMenuItem>
 
--- src/modules/SelectMenuButton.jsx
+++ src/modules/SelectMenuButton.jsx
@@ -2,7 +2,7 @@ import '../styles/button.sass'
 
 import { defineComponent, ref, watch, computed } from 'vue'
 
-import { ROUNDED_PILL } from '../constants'
+import { ROUNDED_PILL, ROUNDED_CIRCLE } from '../constants'
 import { getButtonRoundedClass } from '../core/helper'
 
 export default defineComponent({
@@ -15,13 +15,15 @@ export default defineComponent({
     size: { type: String, default: '' }
   },
   setup (props, { emit, slots }) {
+    const isCircle = computed(() => props.rounded === ROUNDED_CIRCLE)
+
     function getSizeClass () {
       if (!['small', 'mini'].includes(props.size)) return ''
       return `select-menu--btn-${props.size}`
     }
     const classes = computed(() => {
       const classProps = {
-        block: props.block,
+        block: props.block && !isCircle.value,
         disabled: props.disabled
       }
       return [
@@ -30,9 +32,29 @@ export default defineComponent({
         getButtonRoundedClass(props.rounded)
       ]
     })
+
+    function ButtonPrepend () {
+      if (!slots.prepend) return null
+      if (isCircle.value) return null
+      return (
+        <div class='select-menu-btn-prepend'>{slots.prepend()}</div>
+      )
+    }
+    function ButtonAppend () {
+      if (!slots.append) return null
+      if (isCircle.value) return null
+      return (
+        <div class='select-menu-btn-append'>{slots.append()}</div>
+      )
+    }
+
     return () => (
       <div class={classes.value}>
-        {slots?.default?.()}
+        <ButtonPrepend />
+        <div class='select-menu-btn-body'>
+          {slots?.default?.()}
+        </div>
+        <ButtonAppend />
       </div>
     )
   }
